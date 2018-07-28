79d596fa0f8c1be9d0d44a2dbd5a4fec8d4018f8
tilark <tilark.liu@gmail.com>
2018-07-28 12:06:25 +0800

    动态生成路由，根据权限显示侧边栏
    1. 添加store的permission
    2. 更改router表，添加role权限限定
    3. 在sidebar中从动态路由处获得路由表显示侧边栏。

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,8 @@ Vue.use(Router)
 
 /* Layout */
 import Layout from '../views/layout/Layout'
-
+import Login from '../views/login/index'
+const dashboard = resolve => require(['../views/dashboard/index'], resolve)
 /**
 * hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
 * alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
@@ -21,57 +22,169 @@ import Layout from '../views/layout/Layout'
     icon: 'svg-name'             the icon show in the sidebar,
   }
 **/
+
+// export const constantRouterMap = [
+//   { path: '/login', component: () => import('@/views/login/index'), hidden: true },
+//   { path: '/404', component: () => import('@/views/404'), hidden: true },
+
+//   {
+//     path: '/',
+//     component: Layout,
+//     redirect: '/dashboard',
+//     name: 'Dashboard',
+//     hidden: true,
+//     children: [{
+//       path: 'dashboard',
+//       component: () => import('@/views/dashboard/index')
+//     }]
+//   },
+
+//   {
+//     path: '/example',
+//     component: Layout,
+//     redirect: '/example/table',
+//     name: 'Example',
+//     meta: { title: 'Example', icon: 'example' },
+//     children: [
+//       {
+//         path: 'table',
+//         name: 'Table',
+//         component: () => import('@/views/table/index'),
+//         meta: { title: 'Table', icon: 'table' }
+//       },
+//       {
+//         path: 'tree',
+//         name: 'Tree',
+//         component: () => import('@/views/tree/index'),
+//         meta: { title: 'Tree', icon: 'tree' }
+//       }
+//     ]
+//   },
+
+//   {
+//     path: '/form',
+//     component: Layout,
+//     children: [
+//       {
+//         path: 'index',
+//         name: 'Form',
+//         component: () => import('@/views/form/index'),
+//         meta: { title: 'Form', icon: 'form' }
+//       }
+//     ]
+//   },
+
+//   {
+//     path: '/nested',
+//     component: Layout,
+//     redirect: '/nested/menu1',
+//     name: 'nested',
+//     meta: {
+//       title: 'nested',
+//       icon: 'nested'
+//     },
+//     children: [
+//       {
+//         path: 'menu1',
+//         component: () => import('@/views/nested/menu1/index'), // Parent router-view
+//         name: 'menu1',
+//         meta: { title: 'menu1' },
+//         children: [
+//           {
+//             path: 'menu1-1',
+//             component: () => import('@/views/nested/menu1/menu1-1'),
+//             name: 'menu1-1',
+//             meta: { title: 'menu1-1' }
+//           },
+//           {
+//             path: 'menu1-2',
+//             component: () => import('@/views/nested/menu1/menu1-2'),
+//             name: 'menu1-2',
+//             meta: { title: 'menu1-2' },
+//             children: [
+//               {
+//                 path: 'menu1-2-1',
+//                 component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+//                 name: 'menu1-2-1',
+//                 meta: { title: 'menu1-2-1' }
+//               },
+//               {
+//                 path: 'menu1-2-2',
+//                 component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+//                 name: 'menu1-2-2',
+//                 meta: { title: 'menu1-2-2' }
+//               }
+//             ]
+//           },
+//           {
+//             path: 'menu1-3',
+//             component: () => import('@/views/nested/menu1/menu1-3'),
+//             name: 'menu1-3',
+//             meta: { title: 'menu1-3' }
+//           }
+//         ]
+//       },
+//       {
+//         path: 'menu2',
+//         component: () => import('@/views/nested/menu2/index'),
+//         meta: { title: 'menu2' }
+//       }
+//     ]
+//   },
+
+//   { path: '*', redirect: '/404', hidden: true }
+// ]
 export const constantRouterMap = [
-  { path: '/login', component: () => import('@/views/login/index'), hidden: true },
-  { path: '/404', component: () => import('@/views/404'), hidden: true },
-
-  {
-    path: '/',
-    component: Layout,
-    redirect: '/dashboard',
-    name: 'Dashboard',
-    hidden: true,
-    children: [{
-      path: 'dashboard',
-      component: () => import('@/views/dashboard/index')
-    }]
-  },
-
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'example' },
+  { path: '/login', component: Login, hidden: true },
+  { path: '/', component: Layout, redirect: '/dashboard', name: '首页',
     children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
+      { path: 'dashboard', component: dashboard }
     ]
-  },
+  }
+]
+export default new Router({
+  // mode: 'history', //后端支持可开
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
+})
 
+//  异步挂载的路由
+//  动态需要根据权限加载的路由表
+export const asyncRouterMap = [
   {
     path: '/form',
     component: Layout,
+    meta: { role: ['name', 'super_editor'] },
     children: [
       {
         path: 'index',
         name: 'Form',
         component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
+        meta: { title: 'Form', icon: 'form', role: ['name', 'super_editor'] }
       }
     ]
   },
-
+  {
+    path: '/example',
+    component: Layout,
+    redirect: '/example/table',
+    name: 'Example',
+    meta: { title: 'Example', icon: 'example', role: ['name', 'super_editor'] },
+    children: [
+      {
+        path: 'table',
+        name: 'Table',
+        component: () => import('@/views/table/index'),
+        meta: { title: 'Table', icon: 'table', role: ['name', 'super_editor'] }
+      },
+      {
+        path: 'tree',
+        name: 'Tree',
+        component: () => import('@/views/tree/index'),
+        meta: { title: 'Tree', icon: 'tree', role: ['name', 'super_editor'] }
+      }
+    ]
+  },
   {
     path: '/nested',
     component: Layout,
@@ -129,13 +242,7 @@ export const constantRouterMap = [
       }
     ]
   },
-
-  { path: '*', redirect: '/404', hidden: true }
+  {
+    path: '*', redirct: '/404', hidden: true
+  }
 ]
-
-export default new Router({
-  // mode: 'history', //后端支持可开
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRouterMap
-})
-
